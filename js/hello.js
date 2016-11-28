a4d5ae9ed73cd77e6c970e4961eec28e9a980347
AChmut <alexandr.chmut@gmail.com>
2016-11-28 16:57:09 +0200

    first task. finish 1st task

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,22 @@
-angular.module("myapp", []) .controller("HelloController", function($scope) {
-    $scope.greeting = ""; $scope.update = function() {
+var myapp = angular.module("myapp", []);
+myapp.controller("HelloController", function($scope) {
+
+    $scope.greeting = "";
+    $scope.update = function() {
         if ($scope.name) {
             $scope.greeting = "Hello, "+$scope.name+"!";
-        } }
+        }
+    }
+
+});
+myapp.controller("BirdsController", function($scope) {
+
+    $scope.birds = [
+        {name: 'A', count: 123},
+        {name: 'B', count: 17},
+        {name: 'C', count: '999+'},
+        {name: 'D', count: 447},
+        {name: 'E', count: '999+'},
+        {name: 'F', count: 429}
+    ];
 });
