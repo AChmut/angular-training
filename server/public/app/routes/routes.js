51f185d78e80991dc0d7a623ea6f094ba1d21f8e
AChmut <alexandr.chmut@gmail.com>
2016-11-30 17:59:47 +0200

    task 10: 10.1 - show user form

--- server/public/app/routes/routes.js
+++ server/public/app/routes/routes.js
@@ -3,6 +3,10 @@ var module = angular.module('myapp', ['dndLists', 'ngRoute']);
 module.config(
     function($routeProvider) {
         $routeProvider.
+            when('/register', {
+                templateUrl: 'app/routes/userForm/userForm.html',
+                controller: 'UserFormController'
+            }).
             when('/:section?', {
                 templateUrl: 'app/routes/notes/notes.html',
                 controller: 'NotesController'
