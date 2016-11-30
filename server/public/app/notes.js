0a9463df84a3cc5af46b89267bac71e12285b9e5
AChmut <alexandr.chmut@gmail.com>
2016-11-30 14:24:49 +0200

    task 7: w/o editing task 8: start

--- server/public/app/notes.js
+++ server/public/app/notes.js
@@ -9,42 +9,32 @@ module.controller('NotesController',
 
 
         var update = function () {
-            var notesUrl = $http.get("/notes", {
+            $http.get("/notes", {
                 params: { sort: $scope.sort }
-            });
-            notesUrl.success(function (notes) {
-                    $scope.error = null;
-                    $scope.notes = notes;
-                }
-            );
-            notesUrl.error(function () {
-                $scope.error = "Can't connect to the server";
-            })
+            }).then(
+                function (response) {
+                        $scope.error = null;
+                        $scope.notes = response.data;
+                    },
+                function (error) {
+                     $scope.error = "Can't connect to the server: " + error;
+                });
         };
 
         $scope.add = function() {
             var note = { text: $scope.text };
-            $http.put("/notes", note) .success(function() {
-                $scope.text = "";
-                update();
-            });
+            $http.put("/notes", note).then(update);
         };
 
         $scope.remove = function(id) {
-            $http.delete("/notes", {params: {id:id}}).success(function() {
-                update();
-            });
+            $http.delete("/notes", {params: {id:id}}).success(update);
         };
 
         $scope.sendToTop = function(id) {
-            $http.post("/notes/sendTotTop", {params: {id:id}}).success(function() {
-                update();
-            });
+            $http.post("/notes/sendTotTop", {params: {id:id}}).success(update);
         };
 
-        $scope.refresh = function() {
-            update();
-        };
+        $scope.refresh = update;
 
         $scope.updateSort = function(sort) {
             $scope.sort = sort;
