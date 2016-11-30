6ede17cbc1091fb6c134b2c6dbf0be5f48ce523c
AChmut <alexandr.chmut@gmail.com>
2016-11-30 16:57:47 +0200

    task 9: 9.2 complete

--- server/public/app/routes/routes.js
+++ server/public/app/routes/routes.js
@@ -7,6 +7,10 @@ module.config(
                 templateUrl: 'app/routes/notes/notes.html',
                 controller: 'NotesController'
             }).
+            when('/section/:name', {
+                templateUrl: 'routes/viewSection/viewSection.html',
+                controller: 'ViewSectionController'
+            }).
             otherwise({
                 redirectTo: '/'
             })
